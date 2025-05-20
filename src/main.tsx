acbf170acf922e5cd8ef2d27c2f2533e8d263d80
xsumaiya00 <xshahis00@vutbr.cz>
2025-05-20 20:53:25 +0200

    Fix: delegate all routing to App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import EarPrompt from "./pages/EarPrompt";
-import EarScan from "./pages/EarScan";
-import Home from "./pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/scan" element={<EarPrompt />} />
-        <Route path="/earscan" element={<EarScan />} />
-        
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
