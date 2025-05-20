770127dd100cdd11486699a931922b868bfa5b19
Sumaiya <xshahis00@gmail.com>
2025-05-20 20:24:29 +0200

    Fix casing issue in UUIDScanner import and usage

---
+++ src/pages/UUIDScannerPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UUIDScanner from '../UUIDScanner';
+
+const UUIDScannerPage: React.FC = () => {
+  return (
+    <div>
+      <h1>🔎 UUID Scanner Page</h1>
+      <UUIDScanner />
+    </div>
+  );
+};
+
+export default UUIDScannerPage;
